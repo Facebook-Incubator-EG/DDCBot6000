ba99846e4b08ba542dec87a39de8fa09da9b8102
Cesar Irad Mendoza <cesar.irad.mendoza@gmail.com>
2020-05-04 01:07:25 -0500

    latest commit

--- DDCBot6000/DDCBot6000/DDCBot6000/DDCBot.cs
+++ DDCBot6000/DDCBot6000/DDCBot6000/DDCBot.cs
@@ -13,6 +13,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Facebook;
 using System.Threading;
+using System.Collections;
 
 namespace DDCBot6000
 {
@@ -76,22 +77,99 @@ namespace DDCBot6000
             Console.WriteLine("Welcome!\nEnter FB Access Token: ");
        
             token = Console.ReadLine().Trim();
-            BotPost post = new BotPost();
-            post.FB_ACCESS_TOKEN = token;
 
+            List<Team> teamList = new List<Team>();
+            ArrayList roster = new ArrayList();
+
+            roster.Add("Carlos Felipe Rodriguez");
+
+            teamList.Add(new Team() { TeamName = "Monarcas Morelia", TeamLeague = "Liga MX", TeamStrength = 65, TeamRoster = roster });       
+            teamList.Add(new Team() { TeamName = "Real Madrid", TeamLeague = "La Liga", TeamStrength = 92, TeamRoster = roster });    
+            teamList.Add(new Team() { TeamName = "LA FC", TeamLeague = "MLS", TeamStrength = 69, TeamRoster = roster });      
+            teamList.Add(new Team() { TeamName = "Puntarenas FC", TeamLeague = "Liga FPD", TeamStrength = 99, TeamRoster = roster });
+            teamList.Add(new Team() { TeamName = "C.D. Olimpia", TeamLeague = "LNFPH", TeamStrength = 60, TeamRoster = roster });
+            teamList.Add(new Team() { TeamName = "Juventus", TeamLeague = "Serie A", TeamStrength = 91, TeamRoster = roster });
+
+
+            MatchSimulation newMatch = new MatchSimulation();
+
+            //Console.WriteLine("\n---Now simulating Monarcas v. LA FC Matches---");
+            //Console.WriteLine("Difference = 0.05 - Probability: 50% per team\n");
+
+            //newMatch.simulateMatch(teamList[0].TeamName, teamList[2].TeamName, teamList[0].TeamStrength, teamList[2].TeamStrength);
+
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    newMatch.simulateMatch(teamList[0].TeamName, teamList[2].TeamName, teamList[0].TeamStrength, teamList[2].TeamStrength);
+            //}
+
+            Console.WriteLine("\n---Now simulating C.D. Olimpia v. Real Madrid Matches---");
+            Console.WriteLine("Difference = 0.71 - Probability: 85% Real Madrid - 15% C.D. Olimpia\n");
+
+            string post = newMatch.simulateMatch(teamList[1].TeamName, teamList[4].TeamName, teamList[1].TeamStrength, teamList[4].TeamStrength);
+
+
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    newMatch.simulateMatch(teamList[1].TeamName, teamList[4].TeamName, teamList[1].TeamStrength, teamList[4].TeamStrength);
+            //}
+
+
+            DDCBot send = new DDCBot();
+            send.postToFbText(token, post);
+
+            Console.WriteLine("\nMatch simulation has been posted!");
+
+            //Console.WriteLine("\n----Now Listing All Teams----\n");
+            //foreach(Team value in teamList)
+            //{
+            //    Console.WriteLine("Name: "+value.TeamName);
+            //    Console.WriteLine("League: "+value.TeamLeague);
+            //    Console.WriteLine("Strength: " + value.TeamStrength + "\n");
+
+            //    //Console.WriteLine("----Team Roster---");
+            //    //foreach(string item in value.TeamRoster)
+            //    //{
+            //    //    Console.WriteLine("-"+ item + "\n");
+            //    //}
+            //}
+        }
+
+        void postToFbText(string token, string caption)
+        {
             try
             {
+                BotPost post = new BotPost();
+                post.FB_ACCESS_TOKEN = token;
                 Task.Run(async () =>
                 {
-                    await post.PublishImageAndMessage(caption, imageSource);
-                }).GetAwaiter().GetResult();         
+                    await post.PublishMessage(caption);
+                }).GetAwaiter().GetResult();
                 Console.WriteLine("DDCBot6000 - Post completed succesfully! uwu");
             }
             catch (Exception)
             {
                 Console.WriteLine("An error ocurred, try again or try another token! unu");
             }
-           
+        }
+
+
+        void postToFbImage(string token, string caption, string imageSource)
+        {
+            try
+            {
+                BotPost post = new BotPost();
+                post.FB_ACCESS_TOKEN = token;
+                Task.Run(async () =>
+                {
+                    await post.PublishImageAndMessage(caption, imageSource);
+                }).GetAwaiter().GetResult();
+                Console.WriteLine("DDCBot6000 - Post completed succesfully! uwu");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("An error ocurred, try again or try another token! unu");
+            }
         }
     }
 }
---
+++ DDCBot6000/DDCBot6000/DDCBot6000/MatchSimulation.cs
@@ -0,0 +1,72 @@
+﻿///<remarks> 
+/// -Cesar I. Mendoza (aberuwu)
+/// File: MatchSimulation.cs
+/// Purpose: Contains classes and methods for the simulation of matches
+///          
+/// </remarks>
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDCBot6000
+{
+    public class Team
+    {
+        private string teamName;
+        private string teamLeague;
+        private int teamStrength;
+        private ArrayList teamRoster = new ArrayList();
+
+        public int TeamStrength { get => teamStrength; set => teamStrength = value; }
+        public string TeamLeague { get => teamLeague; set => teamLeague = value; }
+        public string TeamName { get => teamName; set => teamName = value; }
+        public ArrayList TeamRoster { get => teamRoster; set => teamRoster = value; }
+    }
+
+    class MatchSimulation
+    {
+
+        public string simulateMatch(string team1, string team2, int str1, int str2)
+        {
+            var winChance = 0;
+            if (str1 < str2)
+            {
+                double difference = 100-(((double)str1 / (double)str2) * 100);
+
+                Random rnd = new Random(Guid.NewGuid().GetHashCode());
+                
+                if (difference >= 70 && difference <= 100)   {winChance = 95;}
+                else if(difference >= 50 && difference < 70) {winChance = 75;}
+                else if(difference >= 30 && difference < 50) {winChance = 70;}
+                else if(difference >= 10 && difference < 30) {winChance = 55;}
+                else{winChance = 50;}
+             
+                int result = rnd.Next(0, 100) < winChance ? 1 : 0;
+
+                if (result == 1) { return ($"---Match Simulation---\n{team1} vs. {team2} - {team1} wins!"); }
+                else { return($"---Match Simulation---\n{team1} vs. {team2} - {team2} wins!"); }
+            }
+            else
+            {
+                double difference = 100 - (((double)str2 / (double)str1) * 100);
+
+                Random rnd = new Random(Guid.NewGuid().GetHashCode());
+
+                if (difference >= 70 && difference <= 100)    {winChance = 95;}
+                else if (difference >= 50 && difference < 70) {winChance = 75;}
+                else if (difference >= 30 && difference < 50) {winChance = 70;}
+                else if (difference >= 10 && difference < 30) {winChance = 55;}
+                else{ winChance = 50;}
+
+                int result = rnd.Next(0, 100) < winChance ? 1 : 0;
+
+                if (result == 1) {return($"---Match Simulation---\n{team1} vs. {team2} - {team1} wins!");}
+                else {return($"---Match Simulation---\n{team1} vs. {team2} - {team2} wins!");}
+            }
+        }      
+    }
+}
