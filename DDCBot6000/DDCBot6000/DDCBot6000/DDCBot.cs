cafb142b450c2f53022a7a936325ea067e51682c
Cesar Irad Mendoza <cesar.irad.mendoza@gmail.com>
2020-05-05 23:14:47 -0500

    Latest commit

--- DDCBot6000/DDCBot6000/DDCBot6000/DDCBot.cs
+++ DDCBot6000/DDCBot6000/DDCBot6000/DDCBot.cs
@@ -19,7 +19,10 @@ using System.Collections;
 
 namespace DDCBot6000
 {
-
+    ///<remarks> 
+    /// BotPost
+    /// This class contains methods for "manually" posting to facebook using the endpoint        
+    /// </remarks>
     public class BotPost
     {
         private const string FB_IMAGE_ADDRESS = "https://graph.facebook.com/v6.0/me/photos";
@@ -71,32 +74,26 @@ namespace DDCBot6000
         static void Main(string[] args)
         {
             FacebookClient api = new FacebookClient();
-
-           
+        
             string token;
-            string imageSource = "";
             string caption = "";
-
-            Console.WriteLine("----DDCBot6000----");
-            Console.WriteLine("Welcome!\nEnter FB Access Token: ");
-       
-            token = Console.ReadLine().Trim();
-
-            List<Team> teamList = new List<Team>();
-            ArrayList roster = new ArrayList();
-
-            roster.Add("Carlos Felipe Rodriguez");
-
-            teamList.Add(new Team() { TeamName = "Monarcas Morelia", TeamLeague = "Liga MX", TeamStrength = 65, TeamRoster = roster });       
-            teamList.Add(new Team() { TeamName = "Real Madrid", TeamLeague = "La Liga", TeamStrength = 92, TeamRoster = roster });    
-            teamList.Add(new Team() { TeamName = "LA FC", TeamLeague = "MLS", TeamStrength = 69, TeamRoster = roster });      
-            teamList.Add(new Team() { TeamName = "Puntarenas FC", TeamLeague = "Liga FPD", TeamStrength = 99, TeamRoster = roster });
-            teamList.Add(new Team() { TeamName = "C.D. Olimpia", TeamLeague = "LNFPH", TeamStrength = 60, TeamRoster = roster });
-            teamList.Add(new Team() { TeamName = "Juventus", TeamLeague = "Serie A", TeamStrength = 91, TeamRoster = roster });
-
-
-            MatchSimulation newMatch = new MatchSimulation();
             string path = @"C:\Users\Scooby2\Desktop\mon_real.jpg";
+
+            Console.WriteLine(
+                "----------------DDCBot6000---------------- " + 
+                "\nProgrammed by: Cesar I. Mendoza (aberuwu) " +
+                "\nVersion: 1.0" +
+                "\n------------------------------------------" +
+                "\nWelcome! Bienvenido! Wilkomen! Yokoso! \nSimulate football match -> Post to Facebook -> Done! " +
+                "\n\nEnter FB Access Token: "
+                );
+
+            token = Console.ReadLine().Trim();
+          
+            DDCBot sim = new DDCBot();
+            sim.quickMatch();
+            List<Team> teamList = new List<Team>();
+            MatchSimulation newMatch = new MatchSimulation();
 
             for (int i = 0; i < 3; i++)
             {
@@ -105,24 +102,53 @@ namespace DDCBot6000
 
                 Console.WriteLine("\nMatch Result: " + caption);
 
+                Console.WriteLine("\nAttempting to post to Facebook...");
+
                 if (i == 0)
-                {                              
+                {
                     DDCBot send = new DDCBot();
                     send.postToFbImage(BotPost.FB_PAGE_ID, token, path, caption);
                 }
                 else
                 {
-                   DDCBot send = new DDCBot();
+                    DDCBot send = new DDCBot();
                     send.postToFbText(token, caption);
                 }
-             
+
                 string time = DateTime.Now.ToString("t");
 
                 Console.WriteLine($"\nMatch simulation has been posted to Facebook @ {time}. Next simulation will be run in 5 minutes...");
                 Thread.Sleep(300000);
             }
-         
+
         }
+
+
+        void quickMatch()
+        {
+            string caption = "";
+            List<Team> teamList = new List<Team>();
+            ArrayList roster = new ArrayList();
+
+            roster.Add("Carlos Felipe Rodriguez");
+
+            teamList.Add(new Team() { TeamName = "Monarcas Morelia", TeamLeague = "Liga MX", TeamStrength = 65, TeamRoster = roster });
+            teamList.Add(new Team() { TeamName = "Real Madrid", TeamLeague = "La Liga", TeamStrength = 92, TeamRoster = roster });
+            teamList.Add(new Team() { TeamName = "LA FC", TeamLeague = "MLS", TeamStrength = 69, TeamRoster = roster });
+            teamList.Add(new Team() { TeamName = "Puntarenas FC", TeamLeague = "Liga FPD", TeamStrength = 99, TeamRoster = roster });
+            teamList.Add(new Team() { TeamName = "C.D. Olimpia", TeamLeague = "LNFPH", TeamStrength = 60, TeamRoster = roster });
+            teamList.Add(new Team() { TeamName = "Juventus", TeamLeague = "Serie A", TeamStrength = 91, TeamRoster = roster });
+
+            MatchSimulation newMatch = new MatchSimulation();
+            
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"\n---Now simulating {teamList[0].TeamName} v. {teamList[2].TeamName}---");
+                caption = newMatch.simulateMatch(teamList[0].TeamName, teamList[2].TeamName, teamList[0].TeamStrength, teamList[2].TeamStrength);
+                Console.WriteLine("\nMatch Result: " + caption + "\nSimulation finished at: " + DateTime.Now.ToString("h:mm:ss:ffff tt"));
+            }
+        }
+
 
         void postToFbText(string token, string caption)
         {
@@ -138,7 +164,7 @@ namespace DDCBot6000
             }
             catch (Exception)
             {
-                Console.WriteLine("An error ocurred, try again or try another token! unu");
+                Console.WriteLine("\nAn error ocurred, try again or try another token! UnU");
             }
         }
 
@@ -169,7 +195,8 @@ namespace DDCBot6000
             }
             catch (FacebookApiException ex)
             {
-                Console.WriteLine("An error ocurred, try again or try another token! unu");
+                Console.WriteLine("\nAn error ocurred, try again or try another token! UnU " +
+                    "\n\n-------------Facebook Exception Details-------------");
                 throw;
             }
         }       
--- DDCBot6000/DDCBot6000/DDCBot6000/MatchSimulation.cs
+++ DDCBot6000/DDCBot6000/DDCBot6000/MatchSimulation.cs
@@ -14,6 +14,10 @@ using System.Threading.Tasks;
 
 namespace DDCBot6000
 {
+    ///<remarks> 
+    /// Team Class
+    /// This class contains getters and setters for the teams that will be simulated
+    /// </remarks>
     public class Team
     {
         private string teamName;
@@ -27,45 +31,73 @@ namespace DDCBot6000
         public ArrayList TeamRoster { get => teamRoster; set => teamRoster = value; }
     }
 
+    ///<remarks> 
+    /// MatchSimulation Class
+    /// This class contains methods for the simulation of football matches
+    /// </remarks>
     class MatchSimulation
     {
 
+        ///<remarks> 
+        /// simulateMatch
+        /// Return value: String
+        /// Takes 4 parameters, 2 strings for team name and 2 integers for team strength
+        /// Calculate the team difference against highest strength team and set winning chance
+        /// Generate random outcomes based on said difference and probability
+        /// 100% bug free, I swear you won't find a more efficient way to simulate football matches ;) 
+        /// </remarks>
         public string simulateMatch(string team1, string team2, int str1, int str2)
         {
-            var winChance = 0;
-            if (str1 < str2)
-            {
-                double difference = 100-(((double)str1 / (double)str2) * 100);
+            int winChance = 0;
+            int drawChance = 0;
+            int result = 0;
+            int st1;
+            int st2;
+            double difference = 0;
 
-                Random rnd = new Random(Guid.NewGuid().GetHashCode());
-                
-                if (difference >= 70 && difference <= 100)   {winChance = 95;}
-                else if(difference >= 50 && difference < 70) {winChance = 75;}
-                else if(difference >= 30 && difference < 50) {winChance = 70;}
-                else if(difference >= 10 && difference < 30) {winChance = 55;}
-                else{winChance = 50;}
-             
-                int result = rnd.Next(0, 100) < winChance ? 1 : 0;
+            if (str1 < str2) {  difference = 100-(((double)str1 / (double)str2) * 100); }
+            else { difference = 100 - (((double)str2 / (double)str1) * 100); }
 
-                if (result == 1) { return ($"---Match Simulation---\n{team1} vs. {team2} - {team1} wins! \nScore: unkonwn - unknown"); }
-                else { return($"---Match Simulation---\n{team1} vs. {team2} - {team2} wins! \nScore: unkonwn - unknown"); }
+            Random rnd = new Random(Guid.NewGuid().GetHashCode());
+
+            if (difference >= 70 && difference <= 100) { winChance = 95; st1 = rnd.Next(0, 12); st2 = rnd.Next(0, 1); }
+            else if (difference >= 50 && difference < 70) { winChance = 75; st1 = rnd.Next(0, 9); st2 = rnd.Next(0, 2); }
+            else if (difference >= 30 && difference < 50) { winChance = 70; st1 = rnd.Next(0, 7); st2 = rnd.Next(0, 3); }
+            else if (difference >= 10 && difference < 30) { winChance = 55; st1 = rnd.Next(0, 6); st2 = rnd.Next(0, 4); }
+            else { winChance = 50; drawChance = rnd.Next(0, 2); st1 = rnd.Next(0, 5); st2 = rnd.Next(0, 5); }
+
+            if(drawChance == 0) { result = rnd.Next(0, 100) < winChance ? 1 : 0; }
+            else { result = -1; }
+   
+            if (result == 1) {
+         
+                if (st1 <= st2)
+                {
+                    if(st1-1 == -1)
+                    {
+                        st1 += 1;
+                    }
+                    st2 = rnd.Next(0, st1-1);
+                }
+
+                return ($"---Match Simulation---\n{team1} vs. {team2} - {team1} wins! \nScore: {st1} - {st2}");
             }
-            else
-            {
-                double difference = 100 - (((double)str2 / (double)str1) * 100);
+            else if (result == 0) {
 
-                Random rnd = new Random(Guid.NewGuid().GetHashCode());
+                if (st2 <= st1)
+                {
+                    if (st2 - 1 == -1)
+                    {
+                        st2 += 1;
+                    }
+                    st1 = rnd.Next(0, st2-1);
+                }
 
-                if (difference >= 70 && difference <= 100)    {winChance = 95;}
-                else if (difference >= 50 && difference < 70) {winChance = 75;}
-                else if (difference >= 30 && difference < 50) {winChance = 70;}
-                else if (difference >= 10 && difference < 30) {winChance = 55;}
-                else{ winChance = 50;}
-
-                int result = rnd.Next(0, 100) < winChance ? 1 : 0;
-
-                if (result == 1) {return($"---Match Simulation---\n{team1} vs. {team2} - {team1} wins! \nScore: unkonwn - unknown");}
-                else {return($"---Match Simulation---\n{team1} vs. {team2} - {team2} wins! \nScore: unkonwn - unknown");}
+                return ($"---Match Simulation---\n{team1} vs. {team2} - {team2} wins! \nScore: {st1} - {st2}");
+            }
+            else {           
+                st2 = st1;
+                return ($"---Match Simulation---\n{team1} vs. {team2} - Both teams draw! \nScore: {st1} - {st2}");
             }
         }      
     }
